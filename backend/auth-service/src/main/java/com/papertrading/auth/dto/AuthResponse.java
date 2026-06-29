09b55a3d884fa763623033a1a78f466c827b27ff
papermoney386-arch <papermoney386@gmail.com>
2026-06-29 15:32:35 +0530

    Auth Response DTO

---
+++ backend/auth-service/src/main/java/com/papertrading/auth/dto/AuthResponse.java
@@ -0,0 +1,24 @@
+package com.papertrading.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+    private String id;
+    private String email;
+    private String fullName;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private Long expiresIn;
+}
